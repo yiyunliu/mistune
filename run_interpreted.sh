5877cb62d6496912d6be8d5600d9d4982062de84
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2021-05-19 22:16:00 -0400

    add gitignore

---
+++ run_interpreted.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/home/alsymd/.local/klee-cupa/bin/klee  --watchdog --max-memory=5000 --max-time=1h  --posix-runtime --optimize-array=all --optimize --runtime-build=Release+Asserts --libc=uclibc --link-llvm-lib=/home/alsymd/.local/python-debug-llvm-9.x/lib/libpython3.9.so.1.0.bc /home/alsymd/.local/python-debug-llvm-9.x/bin/python3.9.bc /home/alsymd/scratch/cython/python-packages/mistune/run_mistune2.py
